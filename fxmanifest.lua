389b3ae01ee0ee3db576ddc88b3402bc78e41a5c
Azelout <59646937+Azelout@users.noreply.github.com>
2022-07-21 13:10:05 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Azelout#7304'
+repository 'https://github.com/Azelout/obs_no_reticle
+
+description 'Remove weapons reticles'
+
+-- Made for Obsidian RôlePlay --
