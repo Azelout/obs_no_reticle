6b22954f9df5750668b5ce6cef508c951c66c258
Azelout <59646937+Azelout@users.noreply.github.com>
2022-07-21 13:10:25 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Azelout#7304'
-repository 'https://github.com/Azelout/obs_no_reticle
+repository 'https://github.com/Azelout/obs_no_reticle'
 
 description 'Remove weapons reticles'
 
